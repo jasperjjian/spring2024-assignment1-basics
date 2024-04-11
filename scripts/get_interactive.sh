626496e4318591174472b9b1f9521012924dc627
Jasper Jian <jasperjiancan@gmail.com>
2024-04-11 16:04:56 -0700

    Encoding and decoding tokenizer.

---
+++ scripts/get_interactive.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --partition=batch-cpu
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=8G
+#SBATCH --time=24:00:00
+#SBATCH --nodelist=ad12a3ca-02
+
+sleep 12h
--- tests/adapters.py
+++ tests/adapters.py
@@ -536,7 +536,8 @@ def get_tokenizer(
     Returns:
         A BPE tokenizer that uses the provided vocab, merges, and special tokens.
     """
-    raise NotImplementedError
+    tokenizer = bpe_tokenizer.BPETokenizer(vocab, merges, special_tokens)
+    return tokenizer
 
 
 def run_train_bpe(
